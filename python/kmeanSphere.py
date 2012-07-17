40f581b16df1a40ccf2a765b8c72e5c74f76ef00
Ben Postlethwaite <post.ben.here@gmail.com>
2012-07-16 22:34:44 -0700

    bug in clustering stations, not fixed

--- python/kmeanSphere.py
+++ python/kmeanSphere.py
@@ -13,7 +13,7 @@
 ###########################################################################
 # IMPORTS
 ###########################################################################
-import os, re, time, shutil, sys
+import os, re, time, shutil, sys, json
 from preprocessor import renameEvent, is_number
 from collections import defaultdict
 import numpy as np
@@ -34,12 +34,22 @@ def vectorize(latlon):
                       sin(obs[0]) ]
     return vects
 
+# Sorry dot product for distance???!!!
+# Distance is ||x1-x2||
+# || vects - clusters ||
 def assignVect2Cluster(vects, clusters):
    '''This calculates distances between vects and array of clusters
    producing an array of distances, each row for obs and each col
    for particular cluster, than we choose index of min dist'''
-   members = np.dot(vects, clusters.T).argmin(axis = 1)
+   a = np.zeros( len(clusters) )
+   members = np.zeros( len(vects) )
+   for i, vect in enumerate(vects):
+       for j, cluster in enumerate(clusters):
+           a[j] = sqrt( np.dot( (vect - cluster), (vect - cluster) ))
+           print a
+       members[i] = a.argmin()
    return members
+
 
 def moveClusters(vects, clusters, members):
     ''' selects the vects belonging into a cluster, sums along the
@@ -57,8 +67,12 @@ def devectorize(vects):
     return latlon * 180/pi
 
 
+def clusterEvents(stdir):
 
-def clusterEvents(stdir)
+    if not stdir:
+        print "Please supply full path to target directory"
+        return
+
     reg2 = re.compile(r'^stack_(\w)\.sac')
     events = os.listdir(stdir)
     events = filter(is_number,events)
@@ -77,11 +91,9 @@ def clusterEvents(stdir)
 
     # Default to two clusters, one around japan the other
     # around Chile.
-    clusterlatlon = np.array( [ [35 , 135 ],
-                                [35, -71] ] )
-
     vects = vectorize(data)
-    clusters = vectorize(clusterlatlon)
+    clusters = vectorize( np.array( [ [35 , 135 ],
+                                      [35, -71] ]) )
 
     for i in range(5):
         members = assignVect2Cluster(vects, clusters)
@@ -109,14 +121,58 @@ def clusterStations(stdict, k):
     ''' Clusters stations in k groups and outputs the
     station name followed by it's zone number'''
 
-    data = np.zeros( len(stdict), 2)
+    data = np.zeros( (len(stdict), 2) )
+    stations = []
     ind = 0
     for st in stdict.keys():
-        data[ind] = [ stdict[st]
+        data[ind] = [ float(stdict[st]['lat']) , float(stdict[st]['lon']) ]
+        stations.append(st)
+        ind += 1
+
+    vects = vectorize(data)
+
+    clusterLatLon = np.array( [[49, -123],
+                               [43, -79],
+                               [70, -62],
+                               [60, -135]])
+
+    clusters = vectorize( clusterLatLon )
+
+    for i in range(5):
+        members = assignVect2Cluster(vects, clusters)
+        clusters = moveClusters(vects, clusters, members)
+        print members
+
+    cls = devectorize(clusters)
+    bmap = Basemap(width=8000000,height=5500000,projection='stere',
+                   resolution='c',lat_1=60.,lat_2=70,lat_0=65,lon_0=-107.)
+    #bmap = Basemap(projection = 'robin', lon_0 = -80)
+    bmap.drawmapboundary(fill_color='aqua')
+    #bmap.fillcontinents(color = 'coral', lake_color='aqua')
+    parallels = np.arange(0.,81,10.)
+    bmap.drawparallels(parallels,labels=[False,True,True,False])
+    meridians = np.arange(10.,351.,20.)
+    bmap.drawmeridians(meridians,labels=[True,False,False,True])
+    x1, y1 = bmap( data[:,1], data[:,0] )
+    x2, y2 = bmap( cls[:,1], cls[:,0] )
+
+    clr = ['g','b','r', 'c','y','k','m']
+
+    bmap.scatter(x2, y2, marker = 'x')
+    for i in range(k):
+        bmap.scatter(x1[members == i], y1[members == i], c = clr[i], marker = 'o')
+
+    plt.show()
+
 
 
 if __name__== '__main__' :
 
-    stdir = sys.argv[1]
+    if len(sys.argv) > 1:
+        stdir = sys.argv[1]
+    else:
+        stdir = None
 
-# Need to save all the membership into SAC files.
+    dbfile = os.environ['HOME']+'/thesis/stations.json'
+    stdict = json.loads( open(dbfile).read() )
+    clusterStations(stdict, 4)
