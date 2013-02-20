e966113d694761fe861b2b5d061f4e88a1887023
Ben Postlethwaite <post.ben.here@gmail.com>
2013-02-19 19:21:52 -0800

    presentatoion

--- python/thompsonSnyderComparison.py
+++ python/thompsonSnyderComparison.py
@@ -36,6 +36,10 @@ Rsnyd = []
 R = []
 stdR = []
 stdRsnyd = []
+Hsnyd = []
+H = []
+stdH = []
+stdHsnyd = []
 Rchili = []
 Rjapan = []
 stns = []
@@ -44,6 +48,10 @@ for stn in snyd.keys():
         stns.append(stn)
         Rsnyd.append(float(snyd[stn]['R']))
         R.append(float(stdict[stn]["hk"]["R"]))
+        Hsnyd.append(float(snyd[stn]['H']))
+        H.append(float(stdict[stn]["hk"]["H"]))
+        stdHsnyd.append(float(snyd[stn]['stdH']))
+        stdH.append(float(stdict[stn]["hk"]["stdH"]))
         stdRsnyd.append(float(snyd[stn]['stdR']))
         stdR.append( 2 * float(stdict[stn]["hk"]["stdR"]) )
         Rjapan.append(float(stdict[stn]["hk"]['c0R']))
@@ -58,6 +66,10 @@ R = np.array(R)
 Rsnyd = np.array(Rsnyd)
 stdR = np.array(stdR)
 stdRsnyd = np.array(stdRsnyd)
+stdH = np.array(stdH)
+stdHsnyd = np.array(stdHsnyd)
+H = np.array(H)
+Hsnyd = np.array(Hsnyd)
 stns = np.array(stns)
 Rchili = np.array(Rchili)
 Rjapan = np.array(Rjapan)
@@ -67,18 +79,24 @@ R = R[~ind]
 Rjapan = Rjapan[~ind]
 Rchili = Rchili[~ind]
 Rsnyd = Rsnyd[~ind]
+H = H[~ind]
+Hsnyd = Hsnyd[~ind]
+stdH = stdH[~ind]
+stdHsnyd = stdHsnyd[~ind]
 stdR = stdR[~ind]
 stdRsnyd = stdRsnyd[~ind]
 stns = stns[~ind]
 
 corr = pearsonr(R, Rsnyd)
-print "Correlation between datasets is {0:.2f}".format(corr[0])
+print "Correlation between Vp/Vs datasets is {0:.2f}".format(corr[0])
 
 t = np.arange(len(R))
 
 #######################
 # Plotting formatter
 #######################
+width = 10
+height = width / 1.9
 ratio = 1.5
 lw = 4 / ratio# line width
 ms = 12 / ratio# marker size
@@ -90,11 +108,12 @@ label = 16 / ratio
 title = 18 / ratio
 leg = 16 / ratio
 
+plt.figure(figsize = (width, height))
 ax = plt.subplot(111)
 plt.plot(t, R, '-ob', lw = lw, ms = ms, label = "Vp/Vs estimate -  current data set")
 plt.plot(t, Rsnyd, '-og', lw = lw, ms = ms, label = "Vp/Vs estimate Thompson et al.")
-plt.plot(t, Rjapan, '*',  ms = ms, color = 'orange',  label = "Vp/Vs estimate Japan Source Region")
-plt.plot(t, Rchili, '*',  ms = ms, color = 'yellow', label = "Vp/Vs estimate Chili Source Region")
+# plt.plot(t, Rjapan, '*',  ms = ms, color = 'orange',  label = "Vp/Vs estimate Japan Source Region")
+# plt.plot(t, Rchili, '*',  ms = ms, color = 'yellow', label = "Vp/Vs estimate Chili Source Region")
 plt.errorbar(t, R, yerr=stdR, xerr=None, fmt=None, ecolor = 'blue',
              elinewidth = elw, capsize = caplen, mew = capwid, label = "2 std error")
 plt.errorbar(t, Rsnyd, yerr=stdRsnyd, xerr=None, fmt=None, ecolor = 'green',
@@ -113,4 +132,37 @@ for tick in ax.xaxis.get_major_ticks():
 plt.yticks(size = ticks)
 plt.ylabel('Vp/Vs', size = label)
 plt.grid(True)
+plt.axis("tight")
+
+
+
+plt.figure(figsize = (width, height))
+ax = plt.subplot(111)
+
+corr = pearsonr(H, Hsnyd)
+print "Correlation between H datasets is {0:.2f}".format(corr[0])
+
+plt.plot(t, H, '-ob', lw = lw, ms = ms, label = "H estimate -  current data set")
+plt.plot(t, Hsnyd, '-og', lw = lw, ms = ms, label = "H estimate Thompson et al.")
+plt.errorbar(t, H, yerr = stdH, xerr=None, fmt=None, ecolor = 'blue',
+             elinewidth = elw, capsize = caplen, mew = capwid, label = "2 std error")
+plt.errorbar(t, Hsnyd, yerr = stdHsnyd, xerr=None, fmt=None, ecolor = 'green',
+             elinewidth = elw, capsize = caplen, mew = capwid, label = "2 std error")
+plt.legend(prop={'size': leg})
+# plt.title("Comparison for given Canadian Shield Stations - " +
+#           "Thompson et al (2010) data.\n" +
+#           "Correlation: {0:2.3f}".format(corr[0]),
+#           size = title)
+plt.xticks(t,stns, size = ticks)
+for tick in ax.xaxis.get_major_ticks():
+                tick.label.set_fontsize( ticks )
+                # specify integer or one of preset strings, e.g.
+                #tick.label.set_fontsize('x-small')
+                tick.label.set_rotation('vertical')
+plt.yticks(size = ticks)
+plt.ylabel('Vp/Vs', size = label)
+plt.grid(True)
+plt.axis("tight")
+
+
 plt.show()
