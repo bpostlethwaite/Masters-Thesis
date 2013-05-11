9ac99c1d81cf15f26c472c66c9e5d6b8983fe0ed
Ben Postlethwaite <post.ben.here@gmail.com>
2013-05-11 16:54:49 -0700

    outline ++

--- python/poisson.py
+++ python/poisson.py
@@ -16,11 +16,22 @@ if __name__== '__main__' :
 
     reverse = False
 
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
         if sys.argv[1] == '-r':
             reverse = True
+        else:
+            numbers = [sys.argv[1]]
 
-    numbers = sys.stdin.read().split()
+    if len(sys.argv) == 3:
+        if sys.argv[1] == '-r':
+            reverse = True
+        numbers = [sys.argv[2]]
+
+    if sys.stdin.isatty():
+        pass
+    else:
+        # trick to get all floats out of list
+        numbers =  re.findall(r'[-+]?[0-9]*\.?[0-9]+', sys.stdin.read() )
 
     numbers = map(float, numbers)
 
